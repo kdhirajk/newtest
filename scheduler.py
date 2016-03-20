7f7cc462df77875a9d036e33ab3041e4c7077653
kdhirajk <kdhiraj@zeomega.com>
2016-03-20 18:03:28 +0530

    Correction for scheduler

--- scheduler.py
+++ scheduler.py
@@ -11,7 +11,7 @@ def tick():
 
 if __name__=="__main__":
     sched=BackgroundScheduler(timezone=utc)
-    sched.add_job(tick,cron,hour=10,minute=10,second=10)
+    sched.add_job(tick,'cron',hour=10,minute=10,second=10)
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
     try:
